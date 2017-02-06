affde26044f5c45b8b804e4c4d2b432a113610dd
zcfrank1st <zcfrank1st@sina.com>
2017-02-06 09:41:59 +0800

    project structure init

---
+++ src/main/scala/com/chaos/ark/ArkMain.scala
@@ -0,0 +1,11 @@
+package com.chaos.ark
+
+/**
+  * Created by zcfrank1st on 06/02/2017.
+  */
+object ArkMain {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/main/scala/com/chaos/ark/actors/FileActor.scala
@@ -0,0 +1,8 @@
+package com.chaos.ark.actors
+
+/**
+  * Created by zcfrank1st on 06/02/2017.
+  */
+class FileActor {
+
+}
