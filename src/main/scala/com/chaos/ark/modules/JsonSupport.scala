db283338351db71c642e50762b5a11483bc98dc1
zcfrank1st <zcfrank1st@sina.com>
2017-06-27 18:08:56 +0800

    fix json serial code

--- src/main/scala/com/chaos/ark/modules/JsonSupport.scala
+++ src/main/scala/com/chaos/ark/modules/JsonSupport.scala
@@ -1,6 +1,6 @@
 package com.chaos.ark.modules
 
-import com.chaos.ark.models.Creature
+import com.chaos.ark.models.{Creature, Log}
 import com.google.gson.Gson
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
@@ -9,6 +9,7 @@ import spray.json.RootJsonFormat
   * Created by zcfrank1st on 06/02/2017.
   */
 trait JsonSupport {
+  implicit val logFormat: RootJsonFormat[Log] = jsonFormat9(Log)
   implicit val creatureFormat: RootJsonFormat[Creature] = jsonFormat3(Creature)
 
   val gson = new Gson()
